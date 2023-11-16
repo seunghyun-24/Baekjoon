eb7fbc6be2673c8e2e113dd11cd6d3f6ec2f40e7
Seunghyun Park <98291947+seunghyun-24@users.noreply.github.com>
2023-11-16 19:51:56 +0900

    [level unrated] Title: 문자열 돌리기, Time: 1.61 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    for(int i = 0; i < str.length(); i++){
+        cout << str[i] << '\n';
+    }
+    return 0;
+}
